0349683e83b8b703da1b468bd34416ee1416d61b
zeeshan.muhammad <zeeshan.muhammad@wearemp.com>
2024-07-12 20:22:54 +0100

    added detail implementation

---
+++ detail/src/main/java/com/genxsol/detail/presentation/uievent/DetailUIEvent.kt
@@ -0,0 +1,8 @@
+package com.example.detail.presentation.uievent
+
+sealed class DetailUIEvent {
+    data object Dismiss : DetailUIEvent()
+    data object LoadItemDetail : DetailUIEvent()
+    data object SearchDetailClick: DetailUIEvent()
+
+}
